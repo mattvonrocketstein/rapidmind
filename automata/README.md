ffcb7d2c9d4971a257f12a207e1966b162577a3e
mvr <matthewvonrocketstein@gmail-dot-com>
2016-08-26 14:30:43 +0000

    flush old projects

--- automata/README.md
+++
@@ -1,4 +0,0 @@
-Automata
-========
-
-** TODO: Add description **
--- rasp/.iex.exs
+++
@@ -1,8 +0,0 @@
-# source another `.iex.exs` file
-#import_file "~/.iex.exs"
-
-# print something before the shell starts
-IO.puts "hello world from .iex.exs"
-
-# bind a variable that'll be accessible in the shell
-#value = 13
